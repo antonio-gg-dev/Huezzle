4f5ff44a42ff3feeaba3d7f8f7d146518f95f7ef
katarn <katarn@katarn.es>
2023-07-03 23:14:05 +0200

    Prototyping GameBoard component

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,30 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <GameBoard
+    :board="board"
+  />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
+import GameBoard from '@/components/GameBoard.vue'
+import { Board } from '@/entities/Board'
+import { Cell } from '@/entities/Cell'
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
+    GameBoard
+  },
+  data () {
+    return {
+      board: new Board([
+        [new Cell('#FF3C00'), new Cell('#FF7600'), new Cell('#FFB000'), new Cell('#FFEA00')],
+        [new Cell('#AA6255'), new Cell('#BD8F3F'), new Cell('#D1BC29'), new Cell('#E4E912')],
+        [new Cell('#5588AA'), new Cell('#7CA87D'), new Cell('#A3C751'), new Cell('#CAE725')],
+        [new Cell('#00AEFF'), new Cell('#3AC1BC'), new Cell('#75D37A'), new Cell('#AFE637')]
+      ])
+    }
   }
 })
 </script>
---
+++ src/components/GameBoard.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="board">
+    <template
+      v-for="(row, index) in board.cells"
+      :key="`r-${index}`"
+    >
+      <div
+        v-for="(cell, index) in row"
+        :key="`c-${index}`"
+        class="cell"
+        :style="{
+          backgroundColor: cell.color,
+        } as CSSStyleDeclaration"
+      >
+
+      </div>
+    </template>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue'
+import { Board } from '@/entities/Board'
+
+export default defineComponent({
+  props: {
+    board: {
+      required: true,
+      type: Board as PropType<Board>
+    }
+  }
+})
+</script>
+
+<style scoped lang="scss">
+.board {
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  width: 400px;
+  margin: 0 auto;
+}
+
+.cell {
+  aspect-ratio: 1 / 1
+}
+</style>
