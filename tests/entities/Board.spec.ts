cd03e8a976343b4e53843e64a3330df044c5aea5
katarn <katarn@katarn.es>
2023-07-03 21:05:03 +0200

    Fix test nesting

--- tests/entities/Board.spec.ts
+++ tests/entities/Board.spec.ts
@@ -60,120 +60,120 @@ describe('@/entities/Board', () => {
 
       expect(board.cells).toStrictEqual([[blackCell, blueCell]])
     })
+  })
 
-    describe('movements', () => {
-      it('should start on 0', () => {
-        const board = new Board([[purpleCell]])
+  describe('movements', () => {
+    it('should start on 0', () => {
+      const board = new Board([[purpleCell]])
 
-        expect(board.movements).toBe(0)
-      })
-
-      it('should count movements properly', () => {
-        const board = new Board([
-          [pinkCell, redCell],
-          [yellowCell, orangeCell]
-        ])
-
-        board.swap(0, 1, 0, 0)
-
-        expect(board.movements).toBe(1)
-
-        board.swap(0, 1, 0, 0)
-
-        expect(board.movements).toBe(2)
-
-        board.swap(0, 1, 0, 0)
-
-        expect(board.movements).toBe(3)
-      })
-
-      it('should not count when swap a cell to itself', () => {
-        const board = new Board([[greenCell, yellowCell]])
-
-        board.swap(1, 0, 1, 0)
-
-        expect(board.movements).toBe(0)
-      })
+      expect(board.movements).toBe(0)
     })
 
-    describe('is solved', () => {
-      it('should be true just right after instanced', () => {
-        const board = new Board([
-          [purpleCell, redCell],
-          [blueCell, blackCell]
-        ])
+    it('should count movements properly', () => {
+      const board = new Board([
+        [pinkCell, redCell],
+        [yellowCell, orangeCell]
+      ])
 
-        expect(board.isSolved).toBe(true)
-      })
+      board.swap(0, 1, 0, 0)
 
-      it('should be false if not is in its initial state', () => {
-        const board = new Board([
-          [purpleCell, redCell],
-          [blueCell, blackCell]
-        ])
+      expect(board.movements).toBe(1)
 
-        board.swap(1, 1, 0, 0)
+      board.swap(0, 1, 0, 0)
 
-        expect(board.isSolved).toBe(false)
-      })
+      expect(board.movements).toBe(2)
 
-      it('should be true again if it returns to its initial state', () => {
-        const board = new Board([
-          [purpleCell, redCell],
-          [blueCell, blackCell]
-        ])
+      board.swap(0, 1, 0, 0)
 
-        board.swap(1, 1, 0, 0)
-          .swap(1, 1, 0, 0)
-
-        expect(board.isSolved).toBe(true)
-      })
+      expect(board.movements).toBe(3)
     })
 
-    describe('shuffle', () => {
-      it('should stop being solved after being shuffled', () => {
-        const board = new Board([
-          [whiteCell, yellowCell, greenCell],
-          [pinkCell, orangeCell, blueCell],
-          [purpleCell, redCell, blackCell]
-        ])
+    it('should not count when swap a cell to itself', () => {
+      const board = new Board([[greenCell, yellowCell]])
 
-        board.shuffle()
+      board.swap(1, 0, 1, 0)
 
-        expect(board.isSolved).toBe(false)
-      })
+      expect(board.movements).toBe(0)
+    })
+  })
 
-      it('should be shuffled constantly if shuffled the same day', () => {
-        const firstBoard = new Board([
-          [whiteCell, yellowCell, greenCell],
-          [pinkCell, orangeCell, blueCell],
-          [purpleCell, redCell, blackCell]
-        ])
+  describe('is solved', () => {
+    it('should be true just right after instanced', () => {
+      const board = new Board([
+        [purpleCell, redCell],
+        [blueCell, blackCell]
+      ])
 
-        const secondBoard = new Board([
-          [whiteCell, yellowCell, greenCell],
-          [pinkCell, orangeCell, blueCell],
-          [purpleCell, redCell, blackCell]
-        ])
+      expect(board.isSolved).toBe(true)
+    })
 
-        firstBoard.shuffle()
-        secondBoard.shuffle()
+    it('should be false if not is in its initial state', () => {
+      const board = new Board([
+        [purpleCell, redCell],
+        [blueCell, blackCell]
+      ])
 
-        expect(firstBoard.cells).toStrictEqual(secondBoard.cells)
-      })
+      board.swap(1, 1, 0, 0)
 
-      it('should not shuffle fixed cells', () => {
-        const board = new Board([
-          [fixedCell, yellowCell, greenCell],
-          [pinkCell, orangeCell, blueCell],
-          [purpleCell, redCell, anotherFixedCell]
-        ])
+      expect(board.isSolved).toBe(false)
+    })
 
-        board.shuffle()
+    it('should be true again if it returns to its initial state', () => {
+      const board = new Board([
+        [purpleCell, redCell],
+        [blueCell, blackCell]
+      ])
 
-        expect(board.cells[0][0]).toBe(fixedCell)
-        expect(board.cells[2][2]).toBe(anotherFixedCell)
-      })
+      board.swap(1, 1, 0, 0)
+        .swap(1, 1, 0, 0)
+
+      expect(board.isSolved).toBe(true)
+    })
+  })
+
+  describe('shuffle', () => {
+    it('should stop being solved after being shuffled', () => {
+      const board = new Board([
+        [whiteCell, yellowCell, greenCell],
+        [pinkCell, orangeCell, blueCell],
+        [purpleCell, redCell, blackCell]
+      ])
+
+      board.shuffle()
+
+      expect(board.isSolved).toBe(false)
+    })
+
+    it('should be shuffled constantly if shuffled the same day', () => {
+      const firstBoard = new Board([
+        [whiteCell, yellowCell, greenCell],
+        [pinkCell, orangeCell, blueCell],
+        [purpleCell, redCell, blackCell]
+      ])
+
+      const secondBoard = new Board([
+        [whiteCell, yellowCell, greenCell],
+        [pinkCell, orangeCell, blueCell],
+        [purpleCell, redCell, blackCell]
+      ])
+
+      firstBoard.shuffle()
+      secondBoard.shuffle()
+
+      expect(firstBoard.cells).toStrictEqual(secondBoard.cells)
+    })
+
+    it('should not shuffle fixed cells', () => {
+      const board = new Board([
+        [fixedCell, yellowCell, greenCell],
+        [pinkCell, orangeCell, blueCell],
+        [purpleCell, redCell, anotherFixedCell]
+      ])
+
+      board.shuffle()
+
+      expect(board.cells[0][0]).toBe(fixedCell)
+      expect(board.cells[2][2]).toBe(anotherFixedCell)
     })
   })
 })
