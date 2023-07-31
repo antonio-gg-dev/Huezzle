838863b7a4faff48d49685fa18a0e9d034ee98d3
Chemaclass <chemaclass@outlook.es>
2023-07-31 09:57:45 +0200

    Add de translations to i18n

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,10 +1,11 @@
 import { createI18n } from 'vue-i18n'
 import { en } from '@/lang/en'
 import { es } from '@/lang/es'
+import { de } from '@/lang/de'
 import { bindings } from '@/bindings'
 
 export const i18n = createI18n({
   locale: bindings.settingsRepository.get().getLanguage(),
   fallbackLocale: 'en',
-  messages: { en, es }
+  messages: { en, es, de }
 })
