ed8c159486720b6300a4a07f4f020a438fa7e215
Gerard Juve <31496768+GerardJuv@users.noreply.github.com>
2023-09-26 11:10:53 +0200

    Update i18n.ts

--- src/i18n.ts
+++ src/i18n.ts
@@ -2,10 +2,11 @@ import { createI18n } from 'vue-i18n'
 import { en } from '@/lang/en'
 import { es } from '@/lang/es'
 import { de } from '@/lang/de'
+import { ca } from '@/lang/ca'
 import { bindings } from '@/bindings'
 
 export const i18n = createI18n({
   locale: bindings.settingsRepository.get().getLanguage(),
   fallbackLocale: 'en',
-  messages: { en, es, de }
+  messages: { en, es, de, ca }
 })
